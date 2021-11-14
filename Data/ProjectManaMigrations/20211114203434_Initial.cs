69be66f0e9d1aa9a9c5a0aa71b1b7c1205760c5f
segur <seguro17@gmail.com>
2021-11-14 20:38:39 +0000

    Criação do Model Task (Imcompleto, á espera dos projectos), Criação da base de dados (Imcompleto)

---
+++ Data/ProjectManaMigrations/20211114203434_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FinalProject.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Task",
+                columns: table => new
+                {
+                    TaskId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TaskName = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: false),
+                    Comentary = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TaskState = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Task", x => x.TaskId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Task");
+        }
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+
+        [Required]
+        [StringLength(512)]
+        public string TaskName { get; set; }
+
+        public string Comentary { get; set; }
+
+        public string TaskState { get; set; }
+    }
+
+}
