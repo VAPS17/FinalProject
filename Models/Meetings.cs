99bdafdcf09c61a0590adec5d7907e8c5d35e3f0
goncaloMRsilva <gmrs99.silva@gmail.com>
2021-11-16 18:00:28 +0000

    criada a classe reuniao

---
+++ Models/Meetings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class Meetings
+    {
+        public int MeetingId { get; set; }
+
+        [Required]
+        public DateTime MeetingDateandTime { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string MeetingTopic { get; set; }
+    }
+}
