0a9b1aab9bf259242bcaf5d65384caef82e2f30d
Daniel Carmona <carmona240101@hotmail.com>
2021-11-26 12:16:37 +0000

    cria dados ficticios quando necessario

---
+++ Data/SeedData.cs
@@ -0,0 +1,30 @@
+﻿//#define TEST_PAGINATION_MEMBERS
+
+using FinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Data
+{
+	public class SeedData
+	{
+		internal static void Populate(ProjectManaContext projectManaContext)
+		{
+#if TEST_PAGINATION_MEMBERS
+			for (int i = 1; i <= 1000; i++) {
+				projectManaContext.Member.Add(
+					new Member {
+						Name = "Member " + i,
+						Email = "membertest"+i + "@ipg.pt",
+						EmployeeNumber=""+i
+					}
+				);
+			}
+
+			projectManaContext.SaveChanges();
+#endif
+		}
+	}
+}
