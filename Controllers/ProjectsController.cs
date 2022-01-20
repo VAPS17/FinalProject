ff2d81c92c1bcea730089ab968169f78c05cbe97
goncaloMRsilva <gmrs99.silva@gmail.com>
2022-01-20 18:45:49 +0000

    StartDate dos projetcs esta definino como data do sistema

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -120,6 +120,9 @@ namespace FinalProject.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("ProjectId,Name,Description,ProjectCreator,StartDate,FinishDate,DecisiveDeliveryDate")] Project project)
         {
+
+            project.StartDate = DateTime.Now;
+
             if (project.StartDate >= project.DecisiveDeliveryDate)
             {
                 ModelState.AddModelError("DecisiveDeliveryDate", "Start date is high than Decisive delivery date");
